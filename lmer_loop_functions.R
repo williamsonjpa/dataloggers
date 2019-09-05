91d7baf5380324293097b72a2f7fa4271b4e89f6
Joe Williamson <williamson-joe@hotmail.com>
2019-09-05 18:42:42 +0100

    lmer loop scripts updated and vastly added to.

--- lmer_loop_functions.R
+++ lmer_loop_functions.R
@@ -25,48 +25,151 @@ create_var_combs_list <- function( all_vars ){
 ##   vec_tests: Character vector with all character vectors that you have generated
 ##              using "create_my_vec" function.
 ##   df: Data frame with your data  
-##   response: Character vector of length 1 (response variable)
+##   response_vec: Character vector of response variables
 ##   rand_eff: Character vector of length 1 (random effect)
 
-iterate_lmer <- function( response , rand_eff , vec_tests ,  df ){
+iterate_lmer <- function( response_vec , rand_eff , vec_tests ,  df ){
   
-  # Create tmp vector to store models 
-  tmp.models <- vector( mode = "list", length( vec_tests ) )
-  # This for loop goes through all the elements in vector
-  # "vec_tests", which contain the variables to test for
-  for( i in 1:length( vec_tests ) ){
-
-    #paste response and explanatory variables together
-    model_terms <- c( response , vec_tests[i] )
-    model <- paste0( model_terms, collapse = " ~ " )
+  tmp.all.models <- vector( mode = "list", length( response_vec ))
+  
+  for( j in 1:length( response_vec )){
+    # Create tmp vector to store models 
+    tmp.models <- vector( mode = "list", length( vec_tests ) )
+    # This for loop goes through all the elements in vector
+    # "vec_tests", which contain the variables to test for
+    for( i in 1:length( vec_tests ) ){
+  
+      #paste response and explanatory variables together
+      model_terms <- c( response_vec[j] , vec_tests[i] )
+      model <- paste0( model_terms, collapse = " ~ " )
+      
+      #paste the random effects onto the model
+      model <- c( model , rand_eff )
+      model <- paste0( model , collapse = " + " )
+      
+      cat( "This lmer is gonna take place with vector ", i, "\n",
+           "which is: ", model, "\n")
+      
+      # Run function lme4::lmer with corresponding element
+      # in vector "vec_tests"
+      tmp.models[[ i ]] <- lme4::lmer( formula = model , data = df )
+      names( tmp.models )[[i]] <- paste( response_vec[j] , i , sep='' )
+    }
     
-    #paste the random effects onto the model
-    model <- c( model , rand_eff )
-    model <- paste0( model , collapse = " + " )
-    
-    cat( "This lmer is gonna take place with vector ", i, "\n",
-         "which is: ", model, "\n")
-    
-    # Run function lme4::lmer with corresponding element
-    # in vector "vec_tests"
-    tmp.models[[ i ]] <- lme4::lmer( formula = model , data = df )
-    names( tmp.models )[[i]] <- paste( response , i , sep='' )
+    #save models to list of all models
+    tmp.all.models[[ j ]] <- tmp.models
     
   }
   
-  return( tmp.models )
-
+  
+  names( tmp.all.models ) <- response_vec
+  
+  return( tmp.all.models )
+  
 }
 
-## Function 3: generate AICtable for the model list
+
+
+## Function 3: iterate AICtable for the model list
 ## Arguments:
 ##   model_list: list of the models outputted by function 2 
-generate_AICtab <- function( model_list ){
-  
-  # generate main AIC table 
-  AICtable <- bbmle::AICtab( model_list )
-  summ_table <- do.call( rbind, lapply(model_list, broom::glance))
-  AICtable[["Resid.Dev"]] <- summ_table[["deviance"]]
+# iterate_AICtab <- function( all_model_list ){
+#   
+#   table_list <- vector( mode='list' , length( all_model_list ))
+#   
+#   #iterate over each response model lisdt
+#   for( i in 1:length( all_model_list ) ){
+#     cat( 'Generating AIC tables for ', names( all_model_list )[i] , ' models.' , '\n' )
+#     
+#     # generate main AIC table 
+#     single_model_list <- copy(all_model_list[[ names( all_model_list )[i] ]])
+#     
+#     AICtable <- bbmle::AICtab( single_model_list , weights = TRUE , sort = T )
+#     AICtable <- as.data.frame.ICtab(AICtable)
+#     
+#     summ_table <- do.call( rbind, lapply( single_model_list , broom::glance ))
+#     AICtable[[ "Resid.Dev" ]] <- summ_table[[ "deviance" ]]
+#     AICtable <- as.data.frame.ICtab(AICtable)
+#     
+#     # row names all come out as name of first response variable so heres a fix
+#     response_char <- names( all_model_list )[i]
+#     row_name_subs <- rep( '' , length(all_model_list[i]))
+# 
+#     
+#     for(j in 1:length(row.names(AICtable))){
+#       row_name_subs[j] <- paste0(c(response_char,gsub("[^0-9\\.]", "", x = row.names(AICtable[j,]))),
+#                                         collapse = '') 
+#     }
+#     row.names(AICtable) <- row_name_subs
+#     
+#     
+#     # add AICtable to table_list
+#     table_list[[ i ]] <- AICtable
+#     
+#   }
+#   
+#   names( table_list ) <- names( all_model_list )
+#   return( table_list )
+#   
+# }
 
-  return( AICtable )
+## Function 4: subset the AICtable list based on weight cumulative count
+## Arguments:
+##   cutoff: weight cumulative count cutoff (class num, range 0-1)
+##   table_list: list of ICtab class AICtables
+subset_AICtables <- function( table_list , cutoff ){
+  # for each AICtable
+  for( i in 1:length( table_list )){
+    # uses the function below by BB to change ICtab class to df
+    df <- as.data.frame.ICtab( table_list[[ i ]] )
+    df$cum_weight <- cumsum( df$weight )
+    df <- subset( df , cum_weight <= cutoff )
+    print(df)
+    table_list[[ i ]] <- df
+  }
+  return( table_list )
+}
+
+## Function 5: subset the AICtable list based on weight cumulative count
+## Arguments:
+##   AICtable_list:list of ICtab class AIC tables that have been subsetted by func 4
+##   var_combs_vec: character vector with model combinations
+match_AICtabs_with_exp_vars <- function( AICtable_list , var_combs_vec , exp_vars ){
+  
+  # looop over AIC tables
+  for( i in 1:length( AICtable_list )){
+    
+    # save dataframe as new object
+    df <- AICtable_list[[ i ]]
+    
+    # loop over explanatory variables to make new cols of 0s in df
+    for( j in 1:length( exp_vars )){
+      df[ , exp_vars[ j ] ] <- 0  
+    }
+    
+    for( j in 1:length( df )){
+      # get the model number for each row
+      model_number <- as.integer( gsub( "[^0-9\\.]", "", x = row.names( df[ j , ] ) ) )
+      
+      # add model number to dataframe
+      for( k in 1:length( exp_vars )){
+        
+        # if the model contains a term, change the column in df to 1 on model row
+        if( grepl( exp_vars[ k ] , var_combs_vec[ model_number ] ) == TRUE ){
+          df[ j , exp_vars[ k ] ] <- 1
+          
+        }
+        
+      }
+      
+    }
+    print(df)
+  }
+  return( AICtable_list )
+}
+
+# Ben Bolker's AIC table to dataframe function
+as.data.frame.ICtab <- function(x, row.names = NULL, optional = FALSE, ...){
+  attr(x,"class") <- "data.frame"
+  as.data.frame(x, row.names = row.names, optional = optional)
 }
--- pointData_lmer_loop.R
+++ pointData_lmer_loop.R
@@ -31,7 +31,7 @@ df_lmer <- clean_df( df )
 # TASKS #
 #-------#
 
-# see this location for functions 
+# see these locations for functions 
 source( "lmer_loop_functions.R" )
 source( "pointData_lmer_loop_functions.R" )
 
@@ -54,18 +54,52 @@ for(i in 1:length( var_combs_list )){
   var_combs_vec[i] <- paste(var_combs_list[[i]], collapse = (' + '))
 }
 
-# 5. Call function with vector "vector_test_pars" plus your data 
+# 5. Create a vector of response variables to give to the iterate_lmer function
+resp_var_vec <- c( 'maxTemp' , 'meanTemp' , 'maxVPD' , 'meanVPD' )
+
+# 6. Call function with vector "vector_test_pars" plus your data 
 #    in the second argument of the function
-maxTemp_models <- iterate_lmer(response = 'maxTemp',
+all_model_list <- iterate_lmer(response_vec = resp_var_vec,
                                rand_eff = '(1|riv_poi)',
                                vec_tests = var_combs_vec,
                                df = df_lmer )
 
-# 6. Generate AIC table for the models
-source('lmer_loop_functions.R')
-maxTemp_AICtab <- generate_AICtab( maxTemp_models )
+# 7. Generate AIC table for the models
+# cannot get this to run, will have to return to it at some point
+# all_models_AICtabs <- iterate_AICtab( all_model_list = all_models )
 
-summary(maxTemp_models$maxTemp58)
-summary(maxTemp_models$maxTemp36)
+#for now do it all here
+# make an output list of the tables
+AICtable_list <- vector( mode='list' , length( all_model_list ))
+  
+#iterate over each response model lisdt
+for( i in 1:length( all_model_list ) ){
+  cat( 'Generating AIC tables for ', names( all_model_list )[i] , ' models.' , '\n' )
+  
+  # save the single model list as an object so AICtab doesnt freak out
+  single_model_list <- all_model_list[[ i ]]
+  AICtable <- bbmle::AICtab( single_model_list , weights = TRUE , sort = T )
+
+  # add deviances to AIC tables
+  summ_table <- do.call( rbind, lapply( single_model_list , broom::glance ))
+  AICtable[[ "Resid.Dev" ]] <- summ_table[[ "deviance" ]]
+  
+  # add AIC table to list
+  AICtable_list[[ i ]] <- AICtable
+  
+}
+
+#give the AICtables names in the list
+names( AICtable_list ) <- names( all_model_list )
+
+# 8. subset AICtabs so that only models representing 95% of weight are shown
+AICtable_list <- subset_AICtables( table_list = AICtable_list , cutoff = 0.95 )
+
+# 9. match up the models with their constituent terms 
+AICtable_list <- match_AICtabs_with_exp_vars( AICtable_list = AICtable_list , var_combs_vec = var_combs_vec , 
+                                              exp_vars = vars )
 
 
+# 10. model ouput table.
+source( "lmer_loop_functions.R" )
+all_models_output_table <- generate_output_table( AICtable_list )
